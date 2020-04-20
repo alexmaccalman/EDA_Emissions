91d843618075388fb2b462024f3e9f0c5e9a787c
Maccalman <1018136984@mil>
2020-04-20 13:52:43 -0400

    first commit

---
+++ plot3.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(ggplot2)
+
+## Import data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+
+#summarize data
+county <- NEI %>% filter(fips == "24510")
+Bal <- county %>% group_by(year, type) %>% summarise(total = sum(Emissions))
+
+#plot data
+ggplot(Bal, aes(year, total)) +geom_point() + facet_grid(.~type) + labs(x = "Year", 
+              y = "Total PM2.5 Emissions", title = "Total Annual PM2.5 Emissions by Type", 
+              subtitle = "Baltimore")
+
+dev.copy(png,'plot3.png')
+dev.off()
